7e9240fdd66a4f4013d189218a0043d860baca05
spareluck <jonjardim@gmail.com>
2015-09-10 21:59:53 -0700

    Better UI

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	
 	var count;
 	var correctNum;
+	var userGuess;
+	var userGuessArray = []; 
+	var winnerMsg;
+	var lastGuessDif;
 	newGame();
 
 	// Random Number Generator
@@ -25,89 +29,190 @@ $(document).ready(function(){
   		randomNumber();
   		userGuessReset();
   		$('#guessList li').remove();
+  		userGuessArray = []; 
   		count = 0;
   		guessCount();
-  	};
+  		hint("Make your Guess!");
+  	}
+
   	//Erase previous guess from text input box
   	function userGuessReset() {
   		$('#userGuess').val('').focus();
   	}
 
-  	// Logic to compare user's guess
-  	function guess () {
-  		var userGuess = $("#userGuess").val();
-	
-		// Make sure the guess is correct input
+  	//Calculate the difference between guesses and correct number
+  	function numDif(num) {
+  		return Math.abs(userGuessArray[userGuessArray.length - num] - correctNum);
+  	}
+
+	// Make sure the guess is correct input
+  	function correctInput() {
+  		userGuess = $("#userGuess").val();
+		var makeGuess = hint("Make your Guess!");
 		if(userGuess === '') {
-  			alert('Please make a guess!');
+  			hint('Please make a guess!');
+  			setTimeout(function () {
+    				makeGuess;
+				}, 2000);
   			userGuessReset();
   			return false
   		}
   		else if(isNaN(userGuess)) {
-  			alert("That's not a number!")
+  			hint("That's not a number!");
+  			setTimeout(function () {
+    				makeGuess;
+				}, 2000);
   			userGuessReset();
   			return false
   		}
-
   		else {
-  			userGuess = Math.floor(userGuess);
+  			return userGuess = Math.floor(userGuess);
   		}
+  	}
+
+  	// Create user numbers guessed list that appears underneath number of guesses
+  	function userGuessList() {
 
 		$('#guessList').append("<li>" + userGuess + "</li>");
-		
-		var userGuessArray = []; 
 
- 		$('#guessList li').each(function() {
+ 		$('#guessList li:last-child').each(function() {
  			userGuessArray.push($(this).text())
  		});
 
  		console.log(userGuessArray)
+  	}
 
- 		count++;
-		guessCount();
+  	//Display hint to user
+  	function hint(string) {
+  		$('#feedback').text(string).css("font-size", "1.25em")
+  	}
 
- 		var numDif = Math.abs(userGuessArray[userGuessArray.length -1] - correctNum);
- 		var guessDif = Math.abs(correctNum - userGuessArray[userGuessArray.length-2])
-		
- 		if(userGuessArray.length > 1 && numDif != 0){
-			if(numDif < guessDif){
-				alert('Warmer!');
-			} 	
-			else {
-				alert('Colder!')
+  	//Compare guess number to correct number and give feedback
+  	function hotOrCold() {
+  		var lastGuessLi = $('#guessList li:last-child');
+		var warmOrCold = $(".hotOrCold");
+ 		var feedback = $('#feedback');
+  		lastGuessDif = numDif(1);
+ 		var lastGuessDif2 = numDif(2);
+ 		if(userGuessArray.length == 1){
+ 			lastGuessLi.css("background-color", "#777");
+ 			hintBox();
+ 		}
+
+ 		if(userGuessArray.length > 1 && lastGuessDif != 0){
+			if(lastGuessDif < lastGuessDif2){
+				//Change color of guess li to orange
+				lastGuessLi.css("background-color", "#e74c3c")
+				
+				//have warmer appear with orange background in hint box 
+				warmOrCold.text("Warmer!").css({
+					display: "block",
+					backgroundColor: "#e74c3c"
+				});
+				feedback.css("display", "none") 
+
+				//have warmer message and color disappear 
+				setTimeout(function () {
+    				warmOrCold.css("display", "none").text('');
+					feedback.css("display", "block")
+				}, 1500);			
+
+				hintBox();
+			} else if (lastGuessDif - lastGuessDif2 == 0) {
+				lastGuessLi.css("background-color", "#777")
+				warmOrCold.text("You already guessed that number!").css({
+					display: "block",
+					backgroundColor: "#cc324b",
+					fontSize: "1.25em",
+					padding: "1em 0.4em"
+				});
+				feedback.css("display", "none");
+
+				//have message and color disappear 
+				setTimeout(function () {
+    				warmOrCold.css("display", "none").text('');
+					feedback.css("display", "block")
+				}, 2500);
+				count -= 1;
+				guessCount();
 			}
-		}
+			else {
+				warmOrCold.text("Colder!").css({
+					display: "block",
+					backgroundColor: "#1a4e95"
+				});
+				feedback.css("display", "none") 
 
-		// >= 50 Ice Cold
-		if(numDif >= 50) {
-			alert("Ice Cold. You're more than 50 numbers off.");
+				//have warmer message and color disappear 
+				setTimeout(function () {
+    				warmOrCold.css("display", "none").text('');
+					feedback.css("display", "block")
+				}, 1500);			
+
+				hintBox();
+			} 
 		}
-		// 30 - 49 Cold
-		else if(numDif >= 30 && numDif <= 49) {
-			alert("Cold. You're between 30-49 numbers off.");
+		hintBox();
+	}
+	// Use calculated difference to give appropriate hint 
+	function hintBox() {
+		if(lastGuessDif >= 50) {
+			hint("Ice Cold: More than 50 numbers off");
 		}
-		// 20-29 warm
-		else if(numDif >= 20 && numDif <= 29) {
-			alert("Warm. You are between 20-29 numbers off.");
+		else if(lastGuessDif >= 30 && lastGuessDif <= 49) {
+			hint("Cold: Between 30-49 numbers off");
 		}
-		// 10-19 hot
-		else if(numDif >= 10 && numDif <= 19) {
-			alert("Hot. You're between 10-19 numbers off.");
+		else if(lastGuessDif >= 20 && lastGuessDif <= 29) {
+			hint("Warm: Between 20-29 numbers off");
 		}
-		// 1-10 smoking hot
-		else if(numDif >= 1 && numDif <= 9) {
-			alert("Smoking hot. You are between 1-9 numbers off! You're almost there!");
+		else if(lastGuessDif >= 10 && lastGuessDif <= 19) {
+			hint("Hot: Between 10-19 numbers off");
 		}
-		else if (numDif === 0) {
-			alert("That's correct. The number is: " + correctNum + ". Congratulations! Number of guesses: " + count);
+		else if(lastGuessDif >= 1 && lastGuessDif <= 9) {
+			hint("Smoking Hot: Only 1-9 numbers off!");
+		}
+		else if (userGuess == correctNum){
+			correctGuess();
 			newGame();
 			return false;
 		}
+		return false
+	}
+  	
+  	function guess() {
+  		
+		if(correctInput()) {
+			userGuessList();
 
-		userGuessReset();
-		return false;
+			count++;
+			guessCount();
+
+ 			hotOrCold();
+
+			userGuessReset();
+			return false;
+  		}
   	}
 
+  	// Display message when user guesses # correctly
+  	function correctGuess() {
+  		var correct = "You've guessed the correct number: "
+  		var guessNum = "Number of guesses: "
+  		
+  		$(".appended").html("<p>" + correct + correctNum + "</p>" + "<p>" + guessNum + count + "</p>");
+
+  		$(".appear").fadeIn(1000);
+		$(".appear").delay(5000).fadeOut(1000);
+	}	
+
+  	// Blink congrats when user wins
+  	function blinker() {
+    	$('.blink').fadeOut(500);
+   		$('.blink').fadeIn(500);
+	}	
+	setInterval(blinker, 1000); //Runs every second
+  	
+  	// Functionality of guess button
   	$('#guessButton').on('click', guess);
 
   	// Function to Click on New Game button class = new
